dacffcb1a652eaf92ee2d59d7a67eb65548ff06c
Leokim77-1 <168464066+Leokim77-1@users.noreply.github.com>
2024-12-12 21:06:48 +0000

    Add files via upload

---
+++ Main.js
@@ -0,0 +1,18 @@
+function receipt(){
+window.location.href = 'Receipt.html';
+}
+
+
+function qrcode(){
+window.location.href = 'Qrgen.html';
+}
+
+
+function scan(){
+window.location.href = 'scan.html';
+}
+
+function file(){
+window.location.href = 'editor.html';
+};
+
